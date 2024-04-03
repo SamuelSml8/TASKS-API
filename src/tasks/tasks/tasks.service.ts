82ec70f198c090d4bc5436b9bbe61afcedc76556
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-03 08:56:53 -0500

    Method get to get all tasks

--- src/tasks/tasks/tasks.service.ts
+++ src/tasks/tasks/tasks.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from './tasks.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  //*   METHODS TO EXPORT CONTROLLER
+
+  //   GET METHOD TO OBTAIN ALL TASKS
+  findAll() {
+    return this.taskModel.find().exec();
+  }
+}
