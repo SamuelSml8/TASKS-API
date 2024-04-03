106772520571893d2f34cb27d9f1da72574d7d87
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-03 08:10:07 -0500

    Database connection, entity completed and files app.module and tasks.module finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://samuel8:t4ImxkPNMXAhIrLt@users-nodejs.eyxgrp5.mongodb.net/test.tasksAPI',
+    ),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
